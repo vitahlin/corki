9b0e9370bfd5d9accabf4bff2e075f9e949fba94
Vitah <linw1225@gmail.com>
2018-10-12 19:14:39 +0800

    Feat：socket相关函数封装

--- src/lib/unp.h
+++ src/lib/unp.h
@@ -1,11 +1,20 @@
 // socket函数需要头文件
 #include <netinet/in.h>
 
+// htonl函数需要头文件
+#include <arpa/inet.h>
+
+// bind、listen函数需要头文件
+#include<sys/types.h>
+#include<sys/socket.h>
+
 #include <iostream>
 using namespace std;
 
 
 #define MAX_LINE 4096
+
+#define LISTENQ 1024
 
 // 错误打印函数封装，封装在log_err.cpp文件
 void LogErrQuit(string str);
@@ -13,3 +22,5 @@ void LogErr(string str);
 
 // socket相关函数封装，在wrap_socket.cpp文件
 int Socket(int domain, int type, int protocol);
+void Listen(int sock_fd, int backlog);
+void  Bind(int sock_fd, const struct sockaddr * addr, int sock_len);
--- src/lib/wrap_socket.cpp
+++ src/lib/wrap_socket.cpp
@@ -5,8 +5,32 @@ int Socket(int domain, int type, int protocol)
 	int n;
 	if ((n = socket(domain, type, protocol)) < 0)
 	{
-		LogErrQuit("socker error");
+		LogErrQuit("Socket error.");
 	}
 
 	return n;
+}
+
+void  Bind(int sock_fd, const struct sockaddr * addr, int sock_len)
+{
+	if ( bind(sock_fd, addr, sock_len) < 0)
+	{
+		LogErrQuit("Bind error.");
+	}
+}
+
+void Listen(int sock_fd, int backlog)
+{
+	char *ptr;
+
+	// 允许管理员自己设置backlog的值，atoi函数将字符串转为整型值
+	if ((ptr = getenv("LISTENQ")) != NULL)
+	{
+		backlog = atoi(ptr);
+	}
+
+	if (listen(sock_fd, backlog) != 0)
+	{
+		LogErrQuit("Listen Error.");
+	}
 }
