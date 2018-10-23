589326cfc33ef89d084c74ae17264adcac522a8d
Vitah <linw1225@gmail.com>
2018-10-23 17:32:27 +0800

    Fix：删除错误代码

--- src/chapter5/tcp_cli_1.cpp
+++ src/chapter5/tcp_cli_1.cpp
@@ -1,47 +1,31 @@
 
+#include "./../lib/constant.h"
 #include "./../lib/unp.h"
 
-void StringCli(FILE *fp, int sockfd)
-{
-	char    sendline[MAXLINE], recvline[MAXLINE];
-
-	while (Fgets(sendline, MAXLINE, fp) != NULL)
-	{
-
-		Writen(sockfd, sendline, strlen(sendline));
-
-		if (Readline(sockfd, recvline, MAXLINE) == 0)
-		{
-			LogErrQuit(""str_cli: server terminated prematurely");
-		}
-
-		Fputs(recvline, stdout);
-	}
+void StringCli(FILE *fp, int sockfd) {
 }
 
-int main(int argc, char *argv[])
-{
-	int n;
-	int sock_fd;
-	char send_str[MAXLINE];
-	char receive_str[MAXLINE];
-	struct sockaddr_in sock_addr;
+int main(int argc, char *argv[]) {
+    int n;
+    int sock_fd;
+    char send_str[MAXLINE];
+    char receive_str[MAXLINE];
+    struct sockaddr_in sock_addr;
 
-	if (argc != 2)
-	{
-		LogErrQuit("Param error.");
-	}
+    if (argc != 2) {
+        LogErrQuit("Param error.");
+    }
 
-	sock_fd = Socket(AF_INET, SOCK_STREAM, 0);
-	bzero(&sock_addr, sizeof(sock_addr));
+    sock_fd = Socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&sock_addr, sizeof(sock_addr));
 
-	sock_addr.sin_family = AF_INET;
-	sock_addr.sin_port = htons(9876);
-	InetPton(AF_INET, argv[1], &sock_addr.sin_addr);
+    sock_addr.sin_family = AF_INET;
+    sock_addr.sin_port = htons(9876);
+    InetPton(AF_INET, argv[1], &sock_addr.sin_addr);
 
-	Connect(sock_fd, (struct sockaddr *)&sock_addr, sizeof(sock_addr));
+    Connect(sock_fd, (struct sockaddr *)&sock_addr, sizeof(sock_addr));
 
-	StringCli(stdin, sock_fd);
+    StringCli(stdin, sock_fd);
 
-	return 0;
+    return 0;
 }
