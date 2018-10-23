5c23d21026dcb3c22b61b0984ba5d34d493bbf79
Vitah <linw1225@gmail.com>
2018-10-23 10:06:09 +0800

    Style：格式更新

--- src/lib/log_err.cpp
+++ src/lib/log_err.cpp
@@ -1,12 +1,10 @@
 #include "./unp.h"
 
-void LogErrQuit(string str)
-{
-	cout << str << endl;
-	exit(0);
+void LogErrQuit(string str) {
+    cout << str << endl;
+    exit(0);
 }
 
-void LogErr(string str)
-{
-	cout << str << endl;
+void LogErr(string str) {
+    cout << str << endl;
 }
--- src/lib/wrap_unix.cpp
+++ src/lib/wrap_unix.cpp
@@ -1,30 +1,24 @@
 
 #include "./unp.h"
 
-void Write(int fd, const void *buff, int count)
-{
-	int n;
-	if ((n = write(fd, buff, count)) != count)
-	{
-		LogErrQuit("Write error");
-	}
+void Write(int fd, const void *buff, int count) {
+    int n;
+    if ((n = write(fd, buff, count)) != count) {
+        LogErrQuit("Write error");
+    }
 }
 
-void Close(int fd)
-{
-	if (close(fd) == -1)
-	{
-		LogErrQuit("Close error.");
-	}
+void Close(int fd) {
+    if (close(fd) == -1) {
+        LogErrQuit("Close error.");
+    }
 }
 
-int Fork()
-{
-	int pid;
-	if ( (pid = fork()) == -1)
-	{
-		LogErrQuit("Fork error.");
-	}
+int Fork() {
+    int pid;
+    if ((pid = fork()) == -1) {
+        LogErrQuit("Fork error.");
+    }
 
-	return pid;
+    return pid;
 }
