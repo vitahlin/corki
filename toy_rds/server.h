d15c8f24f0d93a9e698760bac28b4fee456fea35
vitahlin <linw1225@gmail.com>
2022-12-15 20:45:42 +0800

    feat rds

---
+++ toy_rds/server.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+#define AE_NONE 0
+#define AE_READ 1
+#define AE_WRITE 2
--- toy_rds/toy_rds_srv.c
+++
@@ -1,9 +0,0 @@
-/**
- * 返回时间的服务器程序
- */
-#include <stdio.h>
-
-int main(int argc, char **argv) {
-    printf("Hello toy_rds server");
-    return 0;
-}
