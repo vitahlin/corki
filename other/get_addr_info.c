ceccb4343b28dc121714e455f8b2055e8b94f2f1
vitah <vitahlin@gmail.com>
2025-04-01 23:19:27 +0800

    get_addr_info函数

--- other/get_addr_info.c
+++ other/get_addr_info.c
@@ -2,9 +2,57 @@
 #include <netdb.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <netdb.h>
+#include <sys/socket.h>
 
-int main() {
+void getCanUsedIpAddressInLocalhost() {
+    struct addrinfo *ailist, *aip;
+    struct addrinfo hint;
+    struct sockaddr_in *sinp;
+    char *hostname = "localhost";
+    char buf[INET_ADDRSTRLEN];
+    char *server = "6543"; /* 这是服务端口号 */
+    const char *addr;
+    int ilRc;
 
+    hint.ai_family = AF_UNSPEC; /* hint 的限定设置 */
+    hint.ai_socktype = 0; /* 这里可是设置 socket type . 比如 SOCK——DGRAM */
+    hint.ai_flags = AI_PASSIVE; /* flags 的标志很多 。常用的有AI_CANONNAME; */
+    hint.ai_protocol = 0; /* 设置协议 一般为0，默认 */
+    hint.ai_addrlen = 0; /* 下面不可以设置，为0，或者为NULL */
+    hint.ai_canonname = NULL;
+    hint.ai_addr = NULL;
+    hint.ai_next = NULL;
+    ilRc = getaddrinfo(hostname, server, &hint, &ailist);
+    if (ilRc < 0) {
+        printf("str_error = %s\n", gai_strerror(errno));
+        return;
+    }
+
+    /* 显示获取的信息 */
+    for (aip = ailist; aip != NULL; aip = aip->ai_next) {
+        sinp = (struct sockaddr_in *) aip->ai_addr;
+        addr = inet_ntop(AF_INET, &sinp->sin_addr, buf, INET_ADDRSTRLEN);
+        printf(" addr = %s, port = %d\n", addr ? addr : "unknow ", ntohs(sinp->sin_port));
+    }
+}
+
+void getIpByHostname() {
     struct addrinfo hints, *result, *p;
     int status;
 
@@ -13,13 +61,12 @@ int main() {
 
     // 不限制IPv4 IPv6
     hints.ai_family = AF_UNSPEC;
-
     // TCP套接字类型
     hints.ai_socktype = SOCK_STREAM;
 
     if ((status = getaddrinfo("www.baidu.com", "http", &hints, &result)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        return 1;
+        return;
     }
 
     // 遍历所有返回的地址信息
@@ -41,6 +88,14 @@ int main() {
     }
 
     freeaddrinfo(result); // 释放内存
+}
 
+
+int main() {
+    printf("\ngetIpByHostname:\n");
+    getIpByHostname();
+
+    printf("\ngetCanUsedIpAddressInLocalhost:\n");
+    getCanUsedIpAddressInLocalhost();
     return 0;
 }
